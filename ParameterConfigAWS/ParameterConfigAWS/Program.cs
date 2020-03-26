92145f5e870fc7af583bb65c489e658fff779569
YanPershay <yanik22827@gmail.com>
2020-03-26 16:19:55 +0300

    Start with AWS Parameter Store

---
+++ ParameterConfigAWS/ParameterConfigAWS/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using Amazon.SimpleSystemsManagement;
+using Amazon.SimpleSystemsManagement.Model;
+
+namespace ParameterConfigAWS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetConfiguration().Wait();
+        }
+
+        static async Task GetConfiguration()
+        {
+            var region = Amazon.RegionEndpoint.USEast1;
+            var request = new GetParameterRequest()
+            {
+                Name = "/TestParamStore/EnvName"
+            };
+
+            using (var client = new AmazonSimpleSystemsManagementClient(region))
+            {
+                try
+                {
+                    var response = await client.GetParameterAsync(request);
+                    Console.WriteLine($"Parameter {request.Name} value is: {response.Parameter.Value}");
+                }
+                catch (Exception ex)
+                {
+                    Console.Error.WriteLine($"Error: {ex.Message}");
+                }
+            }
+        
+        } 
+    }
+    
+}
